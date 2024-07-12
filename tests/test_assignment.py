935f75be34642e3cbabba5a2f90c2e8078006e7d
Jason Madar <env3d@users.noreply.github.com>
2024-07-12 02:46:58 +0000

    update tests

--- tests/test_assignment.py
+++ tests/test_assignment.py
@@ -1,29 +1,40 @@
 import os
 import re
+import base64
 
 def test_q1_valid_password():
-    content = os.popen('cat passwd_plain.txt | shuf | head -1 | xargs -I{} curl -s --head http://{}@localhost/cgi-bin/q1.sh').read()
-    assert '200 OK' in content
+    idpass = base64.b64encode(b'mark:111111').decode('utf-8')
+    cmd = f'HTTP_AUTHORIZATION="Basic {idpass}" ./q1.sh'
+    content = os.popen(cmd).read()    
+    assert 'ey' in content
 
 def test_q1_bad_password():
-    content = os.popen('curl -s --head http://asdf:asdf@localhost/cgi-bin/q1.sh').read()
-    assert '401 Unauthorized' in content
+    idpass = base64.b64encode(b'aaaa:bbbb').decode('utf-8')
+    cmd = f'HTTP_AUTHORIZATION="Basic {idpass}" ./q1.sh'
+    content = os.popen('./q1.sh').read()
+    assert '401' in content
 
 def test_q1_no_password():
-    content = os.popen('curl -s --head http://localhost/cgi-bin/q1.sh').read()
-    assert '401 Unauthorized' in content
+    content = os.popen('./q1.sh').read()
+    assert '401' in content
 
 def test_q2_valid_password():
-    content = os.popen('curl -s --head http://anna:password@localhost/cgi-bin/q2.sh').read()
-    assert '200 OK' in content
+    idpass = base64.b64encode(b'anna:password').decode('utf-8')
+    cmd = f'HTTP_AUTHORIZATION="Basic {idpass}" ./q2.sh'
+    print(cmd)
+    content = os.popen(cmd).read()
+    assert len(content) > 0
+    assert 'ey' in content, f'{idpass}'
 
 def test_q2_bad_password():
-    content = os.popen('curl -s --head http://asdf:asdf@localhost/cgi-bin/q2.sh').read()
-    assert '401 Unauthorized' in content
+    idpass = base64.b64encode(b'aaaa:bbbb').decode('utf-8')  
+    cmd = f'HTTP_AUTHORIZATION="Basic {idpass}" ./q2.sh'
+    content = os.popen(cmd).read()
+    assert '401' in content
 
 def test_q2_no_password():
-    content = os.popen('curl -s --head http://localhost/cgi-bin/q2.sh').read()
-    assert '401 Unauthorized' in content
+    content = os.popen('./q2.sh').read()
+    assert '401' in content
 
 def test_q3():
     content = os.popen('./q3.sh').read()
